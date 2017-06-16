8e9c1b1f7a3b09e4e3602e103a5556e9fd3d72ac
unknown <jackkykokoko@gmail.com>
2017-06-16 14:25:51 +0800

    second commit

---
+++ seg.py
@@ -0,0 +1,78 @@
+import os
+import SimpleITK as sitk
+import vtk
+
+def Extract3DRA(img_path,vessel_path,bone_path):
+	reader = sitk.ImageFileReader()
+	reader.SetFileName(img_path)
+	img = reader.Execute()
+
+	otsuFilter = sitk.OtsuMultipleThresholdsImageFilter()
+	otsuFilter.SetNumberOfHistogramBins (256)
+	otsuFilter.SetNumberOfThresholds(3)
+	seg = otsuFilter.Execute(img)
+
+	thresholdFilter = sitk.BinaryThresholdImageFilter()
+	thresholdFilter.SetInsideValue (1)
+	thresholdFilter.SetLowerThreshold(3)
+	thresholdFilter.SetOutsideValue (0)
+	thresholdFilter.SetUpperThreshold(3.1)
+	vessel_seg = thresholdFilter.Execute(seg)
+
+	writer = sitk.ImageFileWriter()
+	writer.SetFileName(vessel_path)
+	writer.Execute(vessel_seg)
+
+	thresholdFilter = sitk.BinaryThresholdImageFilter()
+	thresholdFilter.SetInsideValue (1)
+	thresholdFilter.SetLowerThreshold(2)
+	thresholdFilter.SetOutsideValue (0)
+	thresholdFilter.SetUpperThreshold(2.1)
+	bone_seg = thresholdFilter.Execute(seg)
+
+	openingFilter = sitk.BinaryMorphologicalOpeningImageFilter()
+	openingFilter.SetKernelType(1)
+	openingFilter.SetKernelRadius(2)
+	bone_seg = openingFilter.Execute(bone_seg)
+
+	writer = sitk.ImageFileWriter()
+	writer.SetFileName(bone_path)
+	writer.Execute(bone_seg)
+
+def ExtractCBCT(img_path,bone_mask_path,vessel_path):
+	reader = sitk.ImageFileReader()
+	reader.SetFileName(img_path)
+	img = reader.Execute()
+
+	reader.SetFileName(bone_mask_path)
+	bone_mask = reader.Execute()
+
+	otsuFilter = sitk.OtsuMultipleThresholdsImageFilter()
+	otsuFilter.SetNumberOfHistogramBins (256)
+	otsuFilter.SetNumberOfThresholds(4)
+	seg = otsuFilter.Execute(img)
+
+	# vessel_seg = seg
+
+	thresholdFilter = sitk.BinaryThresholdImageFilter()
+	thresholdFilter.SetInsideValue (1)
+	thresholdFilter.SetLowerThreshold(2)
+	thresholdFilter.SetOutsideValue (0)
+	thresholdFilter.SetUpperThreshold(2.1)
+	vessel_seg = thresholdFilter.Execute(seg)
+
+	fillingFilter = sitk.BinaryFillholeImageFilter()
+	vessel_seg = fillingFilter.Execute(vessel_seg)
+
+	writer = sitk.ImageFileWriter()
+	writer.SetFileName(vessel_path)
+	writer.Execute(vessel_seg)
+
+def main():
+	data_folder = "I:/CFD/intracranial CBCT 3DRA/comparison/ChickFK/"
+
+	# Extract3DRA(data_folder + "3DRA/3DRA.nii",data_folder + "3DRA/seg_vessel.nii",data_folder + "3DRA/seg_bone.nii")
+	ExtractCBCT(data_folder + "CBCT/CBCT_resample.nii",data_folder + "3DRA/seg_bone.nii",data_folder + "CBCT/seg_vessel.nii")
+
+if __name__ == "__main__":
+    main()
---
+++ surface_compare.py
@@ -0,0 +1,38 @@
+import os
+import vtk
+
+def SurfaceDistance(threeDRA_path,CBCT_path,output_path):
+	reader3DRA = vtk.vtkSTLReader()
+	reader3DRA.SetFileName(threeDRA_path)
+	reader3DRA.Update()
+	threeDRA = reader3DRA.GetOutput()
+
+	readerCBCT = vtk.vtkSTLReader()
+	readerCBCT.SetFileName(CBCT_path)
+	readerCBCT.Update()
+	CBCT = readerCBCT.GetOutput()
+
+	distanceFilter = vtk.vtkDistancePolyDataFilter()
+	distanceFilter.SetInputData( 0, threeDRA)
+  	distanceFilter.SetInputData( 1, CBCT)
+  	distanceFilter.Update()
+
+  	# extract lcc object
+  	connectivityFilter = vtk.vtkPolyDataConnectivityFilter()
+  	connectivityFilter.SetInputData(distanceFilter.GetOutput())
+	connectivityFilter.Update()
+
+
+
+  	writer = vtk.vtkXMLPolyDataWriter()
+  	writer.SetFileName(output_path)
+  	writer.SetInputData(connectivityFilter.GetOutput())
+  	writer.Update()
+
+def main():
+	data_folder = "I:/CFD/intracranial CBCT 3DRA/comparison/ChanSP/"
+
+	SurfaceDistance(data_folder + '3DRA/surface.stl',data_folder + 'CBCT/surface.stl',data_folder + '/surfaceDistance.vtp')
+
+if __name__ == "__main__":
+    main()
