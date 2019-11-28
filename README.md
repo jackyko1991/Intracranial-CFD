07039f33211bd6c52ebc4ddb8cc70e17ebcbf020
Ka Long Ko <jackkykokoko@gmail.com>
2019-11-28 11:43:11 +0800

    add batch surface extraction

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ A simple tool to perform vessel segmentation based on 3DRA and post contrast CBC
 
 ## Workflow
 1. Batch conversion of DICOM to NIFTI format
-2. Image registration (recommend to use 3D slicer if manual initialization is needed)
+2. Image registration (external process, recommend to use 3D slicer if manual initialization is needed)
 3. Vessel segmentation with mutiple Otsu thresholding
 4. Perform surface comparison
--- auto_vessel_seg.py
+++ auto_vessel_seg.py
@@ -1,5 +1,6 @@
 import os
 import SimpleITK as sitk
+import vtk
 
 def ExtractLargestConnectedComponents(label):
 	ccFilter = sitk.ConnectedComponentImageFilter()
@@ -39,7 +40,7 @@ def Extract3DRA(img_path,vessel_path):
 	thresholdFilter.SetInsideValue (1)
 	thresholdFilter.SetLowerThreshold(3)
 	thresholdFilter.SetOutsideValue (0)
-	thresholdFilter.SetUpperThreshold(3.1)
+	thresholdFilter.SetUpperThreshold(4)
 	vessel_seg = thresholdFilter.Execute(seg)
 
 	vessel_seg = ExtractLargestConnectedComponents(vessel_seg)
@@ -101,13 +102,56 @@ def ExtractCBCT(img_path,vessel_path):
 	writer.SetFileName(vessel_path)
 	writer.Execute(vessel_seg)
 
+def LabelToSurface(label_path, surface_path):
+	print("Converting segmentation label to surface file:",label_path)
+	reader = sitk.ImageFileReader()
+	reader.SetFileName(label_path)
+	label = reader.Execute()
+
+	# binary path
+	bin_path = "D:/Dr_Simon_Yu/CFD_intracranial/code/cxx/label_to_mesh/build/Release/LabelToMesh.exe"
+
+	# convert to binary file
+	command = bin_path + " " + label_path + " " + surface_path + " 1 1"
+	# print(command)
+	os.system(command)
+
+	reader = vtk.vtkGenericDataObjectReader()
+	reader.SetFileName(surface_path)
+	reader.Update()
+	surface = reader.GetOutput()
+
+	transform = vtk.vtkTransform()
+	transform.Scale(-1,-1,1)
+
+	transformFilter = vtk.vtkTransformPolyDataFilter()
+	transformFilter.SetInputData(surface)
+	transformFilter.SetTransform(transform)
+	transformFilter.Update()
+	surface = transformFilter.GetOutput()
+
+	smoothFilter = vtk.vtkSmoothPolyDataFilter()
+	smoothFilter.SetInputData(surface)
+	smoothFilter.SetNumberOfIterations(15);
+	smoothFilter.SetRelaxationFactor(0.1);
+	smoothFilter.FeatureEdgeSmoothingOff();
+	smoothFilter.BoundarySmoothingOn();
+	smoothFilter.Update();
+	surface = smoothFilter.GetOutput()
+
+	writer = vtk.vtkGenericDataObjectWriter()
+	writer.SetFileName(surface_path)
+	writer.SetInputData(surface)
+	writer.Update()
+
 def main():
-	data_folder = "D:/Cloud/Google Drive/intracranial vessels/followup/stent/LingKW/"
+	data_folder = "D:/Dr_Simon_Yu/CFD_intracranial/data/comparison/BlasiRaquelLegaspi/3DRA/"
 
 	# Extract3DRA(data_folder + "baseline/3DRA.nii",data_folder + "baseline/seg_vessel.nii")
 	# Extract3DRA(data_folder + "baseline-post/3DRA.nii",data_folder + "baseline-post/seg_vessel.nii")
 	# Extract3DRA(data_folder + "12months/3DRA.nii",data_folder + "12months/seg_vessel.nii")
-	ExtractCBCT(data_folder + "followup/CBCT.nii",data_folder + "followup/seg_vessel.nii")
+	# ExtractCBCT(data_folder + "followup/CBCT.nii",data_folder + "followup/seg_vessel.nii")
+	LabelToSurface(os.path.join(data_folder,"3DRA_seg.nii.gz"), os.path.join(data_folder,"surface.vtk"))
 
 if __name__ == "__main__":
     main()
