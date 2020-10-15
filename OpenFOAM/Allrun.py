7b6737438938d1c9b78c9c5e41a8c4c950cbd425
Ka Long Ko <jackkykokoko@gmail.com>
2020-10-15 15:02:09 +0800

    update boundary to fit vessel clip domain output

--- OpenFOAM/Allrun.py
+++ OpenFOAM/Allrun.py
@@ -5,10 +5,12 @@ from PyFoam.RunDictionary.ParsedParameterFile import ParsedParameterFile
 import vtk
 import datetime
 import json
+from tqdm import tqdm
+import trimesh
 
 def edit_blockMeshDict(dictionary, stl, edge_buffer=2):
 	try:
-		print("{}: Editing blockMeshDict: {}".format(datetime.datetime.now(),dictionary))
+		tqdm.write("{}: Editing blockMeshDict: {}".format(datetime.datetime.now(),dictionary))
 		blockMeshDict = ParsedBlockMeshDict(dictionary)
 		vertices = blockMeshDict["vertices"]
 
@@ -32,18 +34,18 @@ def edit_blockMeshDict(dictionary, stl, edge_buffer=2):
 		try:
 			blockMeshDict.writeFile()
 		except IOError:
-			print("Can't write file. Content would have been:")
-			print(blockMeshDict)
+			tqdm.write("Can't write file. Content would have been:")
+			tqdm.write(blockMeshDict)
 
 		return 0
 
 	except IOError:
-		print(blockMeshDict_file, "does not exist")
+		tqdm.write(blockMeshDict_file, "does not exist")
 		return 1
 
 def edit_decompseParDict(dictionary, cores=4):
 	try:
-		print("{}: Editing decompseParDict: {}".format(datetime.datetime.now(),dictionary))
+		tqdm.write("{}: Editing decompseParDict: {}".format(datetime.datetime.now(),dictionary))
 		decompseParDict = ParsedParameterFile(dictionary)
 
 		available_core_config = [2,4,8,12]
@@ -70,85 +72,216 @@ def edit_decompseParDict(dictionary, cores=4):
 		try:
 			decompseParDict.writeFile()
 		except IOError:
-			print("Can't write file. Content would have been:")
-			print(decompseParDict)
+			tqdm.write("Can't write file. Content would have been:")
+			tqdm.write(decompseParDict)
 
 	except IOError:
-		print(dictionary, "does not exist")
+		tqdm.write(dictionary, "does not exist")
 		return 1	
 
-def edit_velocity(dictionary, inlet_json, velocity=1):
+def edit_velocity(dictionary, domain_json, velocity=1):
 	"""
 	Edit the initial velocity file for CFD simulation
 
 	:param str dictionary: File path of the velocity dictionary, (0/U)
-	:param str inlet_json: File path of vessel inlet description json
+	:param str domain_json: File path of vessel inlet description json
 	:param float velocity: Inlet velocity magnitude (cm/s)
 	"""
 
 	try:
-		print("{}: Editing velocity file: {}".format(datetime.datetime.now(),dictionary))
+		tqdm.write("{}: Editing velocity file: {}".format(datetime.datetime.now(),dictionary))
 		velocityDict = ParsedParameterFile(dictionary)
 
-		with open(inlet_json, 'r') as f:
-			inlet_dict = json.load(f)
+		with open(domain_json, 'r') as f:
+			domain_dict = json.load(f)
 
-		velocityDict["boundaryField"]["ICA"]["value"] = "uniform (" + \
-			str(inlet_dict["ICA"]["tangent"][0]/100*velocity) + " " + \
-			str(inlet_dict["ICA"]["tangent"][1]/100*velocity) + " " + \
-			str(inlet_dict["ICA"]["tangent"][2]/100*velocity) + ")"
+		boundaryField = {"vessel": {"type": "noSlip"}, "walls":{"type":"empty"}}	
+		for key, value in domain_dict.items():
+			try:
+				if value["type"] == "inlet":
+					boundaryField.update({key: {
+						"type": "fixedValue",
+						"value": "uniform (" +\
+							str(value["tangent"][0]/100*velocity) + " " + \
+							str(value["tangent"][1]/100*velocity) + " " + \
+							str(value["tangent"][2]/100*velocity) + ")"}})
+				elif value["type"] == "outlet":
+					boundaryField.update({key: { 
+						"type": "inletOutlet",
+						"inletValue": "uniform (0 0 0)",
+						"value":"uniform (0 0 0)"}}
+						)
+			except:
+				continue
+
+		velocityDict["boundaryField"] = boundaryField
 
 		try:
 			velocityDict.writeFile()
 		except IOError:
-			print("Can't write file. Content would have been:")
-			print(velocityDict)
+			tqdm.write("Can't write file. Content would have been:")
+			tqdm.write(velocityDict)
 
 	except IOError:
-		print(dictionary, "does not exist")
+		tqdm.write(dictionary, "does not exist")
 		return 1
 
-def edit_snappyHexMeshDict(dictionary, inlet_json):
+def edit_pressure(dictionary, domian_json, pressure=0):
+	"""
+	Edit the initial pressure file for CFD simulation
+
+	:param str dictionary: File path of the velocity dictionary, (0/U)
+	:param str domian_json: File path of vessel inlet description json
+	:param float pressure: Outlet pressure
+	"""
+
 	try:
-		print("{}: Editing snappyHexMeshDict file: {}".format(datetime.datetime.now(),dictionary))
+		tqdm.write("{}: Editing pressure file: {}".format(datetime.datetime.now(),dictionary))
+		pressureDict = ParsedParameterFile(dictionary)
+
+		with open(domian_json, 'r') as f:
+			domain_dict = json.load(f)
+
+		boundaryField = {"vessel": {"type": "zeroGradient"}, "walls":{"type":"empty"}}	
+		for key, value in domain_dict.items():
+			try:			
+				if value["type"] == "inlet":
+					boundaryField.update({key: { 
+						"type": "zeroGradient"}})
+				elif value["type"] == "outlet":
+					boundaryField.update({key: { 
+						"type": "fixedValue",
+						"value":"uniform 0 "}}
+						)
+			except:
+				continue
+
+		pressureDict["boundaryField"] = boundaryField
+
+		try:
+			pressureDict.writeFile()
+		except IOError:
+			tqdm.write("Can't write file. Content would have been:")
+			tqdm.write(pressureDict)
+
+	except IOError:
+		tqdm.write(dictionary, "does not exist")
+		return 1
+
+def edit_snappyHexMeshDict(dictionary, domain_json):
+	try:
+		tqdm.write("{}: Editing snappyHexMeshDict file: {}".format(datetime.datetime.now(),dictionary))
 		snappyHexMeshDict = ParsedParameterFile(dictionary)
 
-		with open(inlet_json, 'r') as f:
-			inlet_dict = json.load(f)
+		with open(domain_json, 'r') as f:
+			domain_dict = json.load(f)
 
+		# clear old things
+		# snappyHexMeshDict["geometry"]["domain_capped.stl"]["regions"] = ""
+
+		# wall
+		regions = {"vessel":{"name": "vessel"}}
+		for key, value in domain_dict.items():
+			try: 
+				if value["type"]:
+					regions.update({key
+						: {"name": key}})
+			except:
+				continue
+
+		snappyHexMeshDict["geometry"]["domain_capped.stl"]["regions"] = regions
+
+		# castellation control
+		regions = {"vessel":{"level":"(3 4)", "patchInfo":{"type": "wall",}}}
+
+		for key, value in domain_dict.items():
+			try:
+				if value["type"] == "inlet" or value["type"] == "outlet":
+					regions.update({key: {"level": "(4 4)", "patchInfo":{"type":"patch"}}})
+			except:
+				continue
+
+		snappyHexMeshDict["castellatedMeshControls"]["refinementSurfaces"]["Geometry"]["regions"] = regions
+
+		# location in mesh
 		snappyHexMeshDict["castellatedMeshControls"]["locationInMesh"] = "(" + \
-			str(inlet_dict["BifurcationPoint"]["coordinate"][0]/1000) + " " + \
-			str(inlet_dict["BifurcationPoint"]["coordinate"][1]/1000) + " " + \
-			str(inlet_dict["BifurcationPoint"]["coordinate"][2]/1000) + ")"
+			str(domain_dict["bifurcation_point"]["coordinate"][0]/1000) + " " + \
+			str(domain_dict["bifurcation_point"]["coordinate"][1]/1000) + " " + \
+			str(domain_dict["bifurcation_point"]["coordinate"][2]/1000) + ")"
 
 		try:
 			snappyHexMeshDict.writeFile()
 		except IOError:
-			print("Can't write file. Content would have been:")
-			print(snappyHexMeshDict)
+			tqdm.write("Can't write file. Content would have been:")
+			tqdm.write(snappyHexMeshDict)
 
 	except IOError:
-		print(snappyHexMeshDict, "does not exist")
+		tqdm.write(snappyHexMeshDict, "does not exist")
 		return 1
+
+def stl_concat(domain_json):
+	if not os.path.exists(domain_json):
+		return
+
+	working_dir = os.path.dirname(domain_json)
+
+	with open(domain_json, 'r') as f:
+		domain_dict = json.load(f)
+
+	if os.path.exists(os.path.join(working_dir,"domain_capped.stl")):
+		os.remove(os.path.join(working_dir,"domain_capped.stl"))
+
+	# vessel wall
+	# check nan and correct
+	mesh = trimesh.load_mesh(os.path.join(working_dir,domain_dict["vessel"]["filename"]))
+	mesh.process()
+	mesh.export(os.path.join(working_dir,domain_dict["vessel"]["filename"]), file_type='stl_ascii')
+
+	stl_text = open(os.path.join(working_dir,domain_dict["vessel"]["filename"])).read().splitlines(True)
+	stl_text[0] = "solid vessel\n"
+	stl_text.append("\n")
+
+	fout = open(os.path.join(working_dir,"domain_capped.stl"), 'w')
+	fout.writelines(stl_text)
+
+	for key, value in domain_dict.items():
+		try:
+			if value["type"]:
+				# check nan and correct
+				mesh = trimesh.load_mesh(os.path.join(working_dir,value["filename"]))
+				mesh.process()
+				mesh.export(os.path.join(working_dir,value["filename"]), file_type='stl_ascii') 
+
+				stl_text = open(os.path.join(working_dir,value["filename"])).read().splitlines(True)
+				stl_text[0] = "solid " + key + "\n"
+				stl_text.append("\n")
+				fout.writelines(stl_text)
+		except:
+			continue
+
+	fout.close()
 
 def run_case(case_dir, output_vtk=False, parallel=True, cores=4):
 	startTime = datetime.datetime.now()
 
-	print("********************************* OpenFOAM CFD Operation *********************************")
-	print("{}: Execute OpenFOAM CFD simulation on directory: {}".format(datetime.datetime.now(),case_dir))
+	tqdm.write("********************************* OpenFOAM CFD Operation *********************************")
+	tqdm.write("{}: Execute OpenFOAM CFD simulation on directory: {}".format(datetime.datetime.now(),case_dir))
+
+	tqdm.write("{}: STL domain merging...".format(datetime.datetime.now()))
+	stl_concat(os.path.join(case_dir,"domain.json"))
 
 	# copy surface from case directory
-	print("{}: Copying necessary files...".format(datetime.datetime.now()))
-	source_file = os.path.join(case_dir,"surface_capped.stl")
-	target_file = "./constant/triSurface/surface_capped.stl"
+	tqdm.write("{}: Copying necessary files...".format(datetime.datetime.now()))
+	source_file = os.path.join(case_dir,"domain_capped.stl")
+	target_file = "./constant/triSurface/domain_capped.stl"
 	shutil.copy(source_file, target_file)
 
-	source_file = os.path.join(case_dir,"inlets.json")
-	target_file = "./constant/inlets.json"
+	source_file = os.path.join(case_dir,"domain.json")
+	target_file = "./constant/domain.json"
 	shutil.copy(source_file, target_file)
 
 	# clean workspace
-	print("{}: Cleaning workspace...".format(datetime.datetime.now()))
+	tqdm.write("{}: Cleaning workspace...".format(datetime.datetime.now()))
 	if os.path.exists("./0/vorticity"):
 		os.remove("./0/vorticity")
 	if os.path.exists("./0/wallShearStress"):
@@ -166,22 +299,23 @@ def run_case(case_dir, output_vtk=False, parallel=True, cores=4):
 			shutil.rmtree(os.path.join("./",folder))
 		except ValueError:
 			continue
+
 	# blockMesh
 	blockMeshDict_file = "./system/blockMeshDict"
-	result = edit_blockMeshDict(blockMeshDict_file, "./constant/triSurface/surface_capped.stl")
+	result = edit_blockMeshDict(blockMeshDict_file, "./constant/triSurface/domain_capped.stl")
 
 	if result == 1:
-		print("blockMeshDict edit fail, case abort")
+		tqdm.write("blockMeshDict edit fail, case abort")
 		return
 
 	# create log dir
 	os.makedirs("./log",exist_ok=True)
 
-	print("{}: Execute blockMesh...".format(datetime.datetime.now()))
+	tqdm.write("{}: Execute blockMesh...".format(datetime.datetime.now()))
 	os.system("blockMesh > ./log/blockMesh.log")
 
 	# extract surface features
-	print("{}: Execute surfaceFeatureExtract...".format(datetime.datetime.now()))
+	tqdm.write("{}: Execute surfaceFeatureExtract...".format(datetime.datetime.now()))
 	os.system("surfaceFeatureExtract > ./log/surfaceFeatureExtract.log")
 
 	if parallel:
@@ -192,19 +326,18 @@ def run_case(case_dir, output_vtk=False, parallel=True, cores=4):
 
 		# decompose the mesh for multicore computation
 		edit_decompseParDict("./system/decomposeParDict", cores=cores)
-		print("{}: Execute decompsePar...".format(datetime.datetime.now()))
+		tqdm.write("{}: Execute decompsePar...".format(datetime.datetime.now()))
 		os.system("decomposePar > ./log/decomposePar.log")
 
 		# need to edit snappyHexMesh file for meshing
-		edit_snappyHexMeshDict("./system/snappyHexMeshDict", "./constant/inlets.json")
-		print("{}: Execute snappyHexMesh in parallel...".format(datetime.datetime.now()))
+		edit_snappyHexMeshDict("./system/snappyHexMeshDict", "./constant/domain.json")
+		tqdm.write("{}: Execute snappyHexMesh in parallel...".format(datetime.datetime.now()))
 		os.system("foamJob -parallel -screen snappyHexMesh -overwrite > ./log/snappyHexMesh.log")
 		# os.system("foamJob -parallel snappyHexMesh -overwrite")
 
 		# reconstruct mesh surface in parallel run
-		print("{}: Reconstructing mesh from parallel run...".format(datetime.datetime.now()))
+		tqdm.write("{}: Reconstructing mesh from parallel run...".format(datetime.datetime.now()))
 		os.system("reconstructParMesh -latestTime -mergeTol 1E-06 -constant > ./log/reconstructParMesh.log")
-
 		os.system("reconstructPar -latestTime > ./log/reconstructPar.log")
 
 		# remove all previously defined parallel outputs
@@ -212,23 +345,25 @@ def run_case(case_dir, output_vtk=False, parallel=True, cores=4):
 			if "processor" in folder:
 				shutil.rmtree(folder)
 
+		# boundary conditions
+		edit_velocity("./0/U", "./constant/domain.json", velocity=32.53)
+		edit_pressure("./0/p", "./constant/domain.json", pressure=0)
+
 		# decompose the mesh for multicore computation
 		edit_decompseParDict("./system/decomposeParDict", cores=cores)
-		print("{}: Execute decompsePar...".format(datetime.datetime.now()))
+		tqdm.write("{}: Execute decompsePar...".format(datetime.datetime.now()))
 		os.system("decomposePar > ./log/decomposePar.log")
 
 		# CFD 
-		edit_velocity("./0/U", "./constant/inlets.json", velocity=32.53)
 		# print("{}: Execute icoFoam in parallel...".format(datetime.datetime.now()))
 		# os.system("foamJob -parallel -screen icoFoam > ./log/icoFoam.log")
-
-		print("{}: Execute pisoFoam in parallel...".format(datetime.datetime.now()))
+		
+		tqdm.write("{}: Execute pisoFoam in parallel...".format(datetime.datetime.now()))
 		os.system("foamJob -parallel -screen pisoFoam > ./log/pisoFoam.log")
 
 		# reconstruct mesh surface in parallel run
-		print("{}: Reconstructing mesh from parallel run...".format(datetime.datetime.now()))
+		tqdm.write("{}: Reconstructing mesh from parallel run...".format(datetime.datetime.now()))
 		os.system("reconstructParMesh -mergeTol 1E-06 -constant > ./log/reconstructParMesh.log")
-
 		os.system("reconstructPar > ./log/reconstructPar.log")
 
 		# remove all previously defined parallel outputs
@@ -238,39 +373,39 @@ def run_case(case_dir, output_vtk=False, parallel=True, cores=4):
 
 	else:
 		# need to edit snappyHexMesh file for meshing
-		edit_snappyHexMeshDict("./system/snappyHexMeshDict", "./constant/inlets.json")
-		print("{}: Execute snappyHexMesh...".format(datetime.datetime.now()))
+		edit_snappyHexMeshDict("./system/snappyHexMeshDict", "./constant/domain.json")
+		tqdm.write("{}: Execute snappyHexMesh...".format(datetime.datetime.now()))
 		os.system("snappyHexMesh -overwrite > ./log/snappyHexMesh.log")
 
 		# run cfd
 		# need to edit initial velocity file
-		edit_velocity("./0/U", "./constant/inlets.json", velocity=32.53)
+		edit_velocity("./0/U", "./constant/domain.json", velocity=32.53)
 		# print("{}: Execute icoFoam...".format(datetime.datetime.now()))
 		# os.system("icoFoam > ./log/icoFoam.log")
 
-		print("{}: Execute pisoFoam...".format(datetime.datetime.now()))
+		tqdm.write("{}: Execute pisoFoam...".format(datetime.datetime.now()))
 		os.system("icoFoam > ./log/pisoFoam.log")
 
-	# post processing
-	print("{}: Computing vorticity...".format(datetime.datetime.now()))
-	os.system("pisoFoam -postProcess -func vorticity > ./log/vorticity.log")
+	# # post processing
+	# tqdm.write("{}: Computing vorticity...".format(datetime.datetime.now()))
+	# os.system("pisoFoam -postProcess -func vorticity > ./log/vorticity.log")
 
-	print("{}: Computing wall shear stress...".format(datetime.datetime.now()))
-	os.system("pisoFoam -postProcess -func wallShearStress > ./log/wallShearStress.log")
+	# tqdm.write("{}: Computing wall shear stress...".format(datetime.datetime.now()))
+	# os.system("pisoFoam -postProcess -func wallShearStress > ./log/wallShearStress.log")
 
 	# create OpenFOAM read fill for paraview
 	os.system("touch OpenFOAM.OpenFOAM")
 
 	# output as vtk file
 	if output_vtk:
-		print("Convert to VTK output")
+		tqdm.write("Convert to VTK output")
 		os.system("foamToVTK > ./log/foamToVTK.log")
 
 	endTime = datetime.datetime.now()
-	print("{}: Auto CFD pipeline complete, time elapsed: {}s".format(datetime.datetime.now(),(endTime-startTime).total_seconds()))
+	tqdm.write("{}: Auto CFD pipeline complete, time elapsed: {}s".format(datetime.datetime.now(),(endTime-startTime).total_seconds()))
 
 	# copy result back to storage node
-	print("{}: Copying result files...".format(datetime.datetime.now()))
+	tqdm.write("{}: Copying result files...".format(datetime.datetime.now()))
 
 	if os.path.exists(os.path.join(case_dir,"CFD_OpenFOAM")):
 		shutil.rmtree(os.path.join(case_dir,"CFD_OpenFOAM"), ignore_errors=True)
@@ -303,23 +438,38 @@ def run_case(case_dir, output_vtk=False, parallel=True, cores=4):
 		tgt_folder = os.path.join(case_dir,"CFD_OpenFOAM","VTK")
 		shutil.copytree(src_folder,tgt_folder)
 
-	print("{}: CFD operation on {} complete".format(datetime.datetime.now(),case_dir))
+	tqdm.write("{}: CFD operation on {} complete".format(datetime.datetime.now(),case_dir))
 
 def main():
-	tx_type = "stent"
+	data_dir = "/mnt/DIIR-JK-NAS/data/intracranial"
+	sub_data_dirs = [
+		"data_ESASIS_followup/medical",
+		"data_ESASIS_followup/stent",
+		"data_ESASIS_no_stenting",
+		"data_surgery",
+		"data_wingspan"]
 
-	data_dir = "/mnt/DIIR-JK-NAS/data/intracranial/followup/{}".format(tx_type)
+	# phases = ["baseline", "baseline-post", "12months", "followup"]
+	phases = ["baseline"]
 
-	phases = ["baseline", "baseline-post", "12months", "followup"]
-	# phases = ["followup"]
+	# datalist = os.listdir(data_dir)
+	datalist = [
+		"ChanPitChuen",
+		"ChanShunPak",
+		"CheungSikNin",
+		"ChickFungKuen",
+		"ChowLaiMing"]
+	
+	pbar = tqdm(datalist)
 
-	for case in os.listdir(data_dir)[0:]:
-	# for case in ["WongLK"]:
-		for phase in phases:
-			if not os.path.exists(os.path.join(data_dir,case,phase)):
-				continue
-			run_case(os.path.join(data_dir,case,phase),output_vtk=True, parallel=True, cores=8)
-			# exit()
+	for sub_data_dir in sub_data_dirs:
+		for case in pbar:
+			pbar.set_description(case)
+			for phase in phases:
+				if not os.path.exists(os.path.join(data_dir,sub_data_dir,case,phase)):
+					continue
+				run_case(os.path.join(data_dir,sub_data_dir,case,phase),output_vtk=True, parallel=True, cores=8)
+			exit()
 
 if __name__ == "__main__":
 	main()
