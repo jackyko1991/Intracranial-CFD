fb808b6027ab0a29045bdf203eb6cd9734d7ff2c
Jacky Ko <jackkykokoko@gmail.com>
2021-02-07 12:24:53 +0800

    add probability compare

---
+++ probability_compare_plot.py
@@ -0,0 +1,13 @@
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def main():
+	csv_list = [
+		"Z:/projects/intracranial/results_with_probability_radius_min.csv",
+		"Z:/projects/intracranial/results_with_probability_selected.csv",
+	]
+
+if __name__ == "__main__":
+	main
